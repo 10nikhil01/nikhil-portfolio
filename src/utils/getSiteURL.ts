df076f8441b0267cff67406aed3c37fc6afb8129
10nikhil01 <nikhilcreationshere@gmail.com>
2025-05-02 18:29:04 +0530

    fix(prettier): prettier issues resolved

--- src/utils/getSiteURL.ts
+++ src/utils/getSiteURL.ts
@@ -2,6 +2,6 @@ import { NEXT_APP_ENVIRONMENT } from "@/config/constant";
 
 export function getSiteUrl(): string {
   return NEXT_APP_ENVIRONMENT === "PRODUCTION"
-        ? "https://nikhil-portfolio-webdev.vercel.app/"
-        : "http://localhost:3000";
+    ? "https://nikhil-portfolio-webdev.vercel.app/"
+    : "http://localhost:3000";
 }

da1ee4758570483c68e291eaa54ab2e4007aabb3
10nikhil01 <nikhilcreationshere@gmail.com>
2025-06-29 14:42:09 +0530

    fix(lint): lint fix for skeleton

--- src/components/common/header/DesktopMenusSkeleton.tsx
+++ src/components/common/header/DesktopMenusSkeleton.tsx
@@ -1,8 +1,8 @@
 function DesktopMenusSkeleton() {
   return (
-    <div className="flex items-center justify-center max-sm:hidden max-lg:mx-auto gap-8 px-6 text-black dark:text-amber-300 py-2 rounded-full font-medium transition-all duration-300 lg:ml-auto  animate-pulse">
+    <div className="flex animate-pulse items-center justify-center gap-8 rounded-full px-6 py-2 font-medium text-black transition-all duration-300 dark:text-amber-300 max-lg:mx-auto max-sm:hidden  lg:ml-auto">
       {Array.from({ length: 4 }).map((_, idx) => (
-        <div key={idx} className="h-5 w-20 bg-muted rounded" />
+        <div key={idx} className="h-5 w-20 rounded bg-muted" />
       ))}
     </div>
   );
